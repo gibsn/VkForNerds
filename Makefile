d55f580e90437993fa4fd25a2808e85e0d36cb26
Kirill Alekseev <kirill-8@yandex.ru>
2016-12-31 21:05:39 +0300

    Implemented logging to file

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ bin/vkfornerds:
 	@which gb > /dev/null || go get github.com/constabulary/gb/...
 	gb build vkfornerds
 
-install: bin/vkfornerds
-	cp bin/vkfornerds $(GOPATH)/bin
-
 clean:
 	rm -rf ./bin/
 	rm -rf ./pkg/
 
-.PHONY: all install clean bin/vkfornerds
+tags:
+	gotags -R ./ > tags
+
+.PHONY: all install clean tags bin/vkfornerds
--- src/vkfornerds/main.go
+++ src/vkfornerds/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+
 	"vkclient"
 )
 
+func RedirectLogsToFile(file string) {
+	if file != "" {
+		file, err := os.OpenFile(file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			log.Println(err)
+			log.Fatalf("Could not open file %s for logging\n", file)
+		}
+		log.SetOutput(file)
+	}
+}
+
 func main() {
 	token := flag.String("t", "", "vk token")
+	logFile := flag.String("o", "", "log to file")
 	flag.Parse()
+
+	RedirectLogsToFile(*logFile)
+
+	if *token == "" {
+		log.Fatal("You have not provided token")
+	}
 
 	client := vkclient.NewVkClient()
 	client.Api.AccessToken = *token
